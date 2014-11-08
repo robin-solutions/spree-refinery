36127062803bb482a15ce11736981776c0b528b8
Ben Robbins <ben.robbins@robinsolutionsllc.com>
2014-11-08 14:40:03 -0500

    Fix register create redirect

--- app/controllers/frontend/spree/user_registrations_controller.rb
+++ app/controllers/frontend/spree/user_registrations_controller.rb
@@ -32,7 +32,8 @@ class Spree::UserRegistrationsController < Devise::RegistrationsController
       sign_in(:refinery_user, @user)
       session[:spree_user_signup] = true
       associate_user
-      respond_with resource, location: after_sign_up_path_for(resource)
+
+      respond_with resource, location: (after_sign_up_path_for(resource) || root_path)
     else
       clean_up_passwords(resource)
       render :new
