22981237bbcbddfd3503c75e3ae48379478a941d
Ben Robbins <ben.robbins@robinsolutionsllc.com>
2014-11-08 20:24:27 -0500

    Tweak redirect override

--- app/decorators/refinery_admin_base_controller_decorator.rb
+++ app/decorators/refinery_admin_base_controller_decorator.rb
@@ -5,7 +5,11 @@ Refinery::Admin::BaseController.class_eval do
     if refinery_user?
       original_refinery_admin_root_path
     else
-      refinery.login_path
+      if main_app.respond_to?(:root_path)
+        main_app.root_path
+      else
+        refinery.root_path
+      end
     end
   end
 end
