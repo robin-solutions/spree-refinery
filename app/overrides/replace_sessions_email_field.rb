bdfa198a07d588fb6b45f4b7bfa83cf3d7af55d4
Ben Robbins <ben.robbins@robinsolutionsllc.com>
2014-11-09 19:20:50 -0500

    Remove bootstrap overrides

--- app/overrides/replace_sessions_email_field.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/shared/_login",
-                     :name => "replace_sessions_email_field",
-                     :replace => "erb[loud]:contains('f.email_field')",
-                     :text => "<%= f.email_field :login, :class => 'title', :tabindex => 1, autofocus: true %>",
-                     :disabled => false)
-                   #  :original => 'eb3fa668cd98b6a1c75c36420ef1b238a1fc55ac')
--- app/overrides/replace_sessions_email_label.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/shared/_login",
-                     :name => "replace_sessions_email_label",
-                     :replace => "erb[loud]:contains('Spree.t(:email)')",
-                     :text => "<%= f.label :login, Spree.t(:email) %>",
-                     :disabled => false)
-                    # :original => 'eb3fa668cd98b6a1c75c36420ef1b238a1fc55ac')
